43488f9a10cec1f86ac4ba5c090e76ea8d7da6ef
Shehan Tis <shehanproductions@ymail.com>
2018-01-28 21:38:41 +0530

    added cors

--- server.js
+++ server.js
@@ -5,13 +5,15 @@
 
 // call the packages we need
 var express = require('express');        // call express
-var app = express();                 // define our app using express
+var app = express();                 // define our app using express+
+var cors = require('cors')
 var bodyParser = require('body-parser');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors())
 
 var port = process.env.PORT || 3535;        // set our port
 
